528622e178c9a951defeb9155b42a1a16fae9e41
SeedSyntax <221134143+SeedSyntax@users.noreply.github.com>
2025-10-23 19:02:28 +0100

    Add UI consumer to API provider Pact tests

---
+++ src/server/common/helpers/sample-data/agreements.js
@@ -0,0 +1,309 @@
+export default [
+  {
+    _id: '68fa51d0206fcc31cb4b4512',
+    notificationMessageId: 'b6cc5590-80f6-46ce-b5a5-14c7986591f4',
+    agreementName: 'Example agreement 2',
+    correlationId: 'c3ef6a9d-1785-4653-894f-d681caef4c4b',
+    clientRef: 'client-ref-002',
+    code: 'frps-private-beta',
+    identifiers: {
+      sbi: '106284736',
+      frn: 'frn',
+      crn: 'crn',
+      defraId: 'defraId',
+      _id: '68fa51d0206fcc31cb4b4513'
+    },
+    status: 'offered',
+    agreement: '68fa51d0206fcc31cb4b450d',
+    scheme: 'SFI',
+    actionApplications: [
+      {
+        code: 'CMOR1',
+        sheetId: 'SD6743',
+        parcelId: '8083',
+        appliedFor: {
+          unit: 'ha',
+          quantity: 4.53411078,
+          _id: '68fa51d0206fcc31cb4b4515'
+        },
+        _id: '68fa51d0206fcc31cb4b4514'
+      }
+    ],
+    payment: {
+      agreementStartDate: '2025-09-01',
+      agreementEndDate: '2028-09-01',
+      frequency: 'Quarterly',
+      agreementTotalPence: 96018,
+      annualTotalPence: 32006,
+      parcelItems: {
+        1: {
+          code: 'CMOR1',
+          description: 'CMOR1: Assess moorland and produce a written record',
+          version: 1,
+          unit: 'ha',
+          quantity: 4.53411078,
+          rateInPence: 1060,
+          annualPaymentPence: 4806,
+          sheetId: 'SD6743',
+          parcelId: '8083',
+          _id: '68fa51d0206fcc31cb4b4517'
+        }
+      },
+      agreementLevelItems: {
+        1: {
+          code: 'CMOR1',
+          description: 'CMOR1: Assess moorland and produce a written record',
+          version: 1,
+          annualPaymentPence: 27200,
+          _id: '68fa51d0206fcc31cb4b4518'
+        }
+      },
+      payments: [
+        {
+          totalPaymentPence: 8007,
+          paymentDate: '2025-12-05',
+          lineItems: [
+            {
+              parcelItemId: 1,
+              paymentPence: 1204,
+              _id: '68fa51d0206fcc31cb4b451a'
+            },
+            {
+              agreementLevelItemId: 1,
+              paymentPence: 6803,
+              _id: '68fa51d0206fcc31cb4b451b'
+            }
+          ],
+          _id: '68fa51d0206fcc31cb4b4519'
+        },
+        {
+          totalPaymentPence: 8001,
+          paymentDate: '2026-03-05',
+          lineItems: [
+            {
+              parcelItemId: 1,
+              paymentPence: 1201,
+              _id: '68fa51d0206fcc31cb4b451d'
+            },
+            {
+              agreementLevelItemId: 1,
+              paymentPence: 6800,
+              _id: '68fa51d0206fcc31cb4b451e'
+            }
+          ],
+          _id: '68fa51d0206fcc31cb4b451c'
+        },
+        {
+          totalPaymentPence: 8001,
+          paymentDate: '2026-06-05',
+          lineItems: [
+            {
+              parcelItemId: 1,
+              paymentPence: 1201,
+              _id: '68fa51d0206fcc31cb4b4520'
+            },
+            {
+              agreementLevelItemId: 1,
+              paymentPence: 6800,
+              _id: '68fa51d0206fcc31cb4b4521'
+            }
+          ],
+          _id: '68fa51d0206fcc31cb4b451f'
+        },
+        {
+          totalPaymentPence: 8001,
+          paymentDate: '2026-09-07',
+          lineItems: [
+            {
+              parcelItemId: 1,
+              paymentPence: 1201,
+              _id: '68fa51d0206fcc31cb4b4523'
+            },
+            {
+              agreementLevelItemId: 1,
+              paymentPence: 6800,
+              _id: '68fa51d0206fcc31cb4b4524'
+            }
+          ],
+          _id: '68fa51d0206fcc31cb4b4522'
+        },
+        {
+          totalPaymentPence: 8001,
+          paymentDate: '2026-12-07',
+          lineItems: [
+            {
+              parcelItemId: 1,
+              paymentPence: 1201,
+              _id: '68fa51d0206fcc31cb4b4526'
+            },
+            {
+              agreementLevelItemId: 1,
+              paymentPence: 6800,
+              _id: '68fa51d0206fcc31cb4b4527'
+            }
+          ],
+          _id: '68fa51d0206fcc31cb4b4525'
+        },
+        {
+          totalPaymentPence: 8001,
+          paymentDate: '2027-03-05',
+          lineItems: [
+            {
+              parcelItemId: 1,
+              paymentPence: 1201,
+              _id: '68fa51d0206fcc31cb4b4529'
+            },
+            {
+              agreementLevelItemId: 1,
+              paymentPence: 6800,
+              _id: '68fa51d0206fcc31cb4b452a'
+            }
+          ],
+          _id: '68fa51d0206fcc31cb4b4528'
+        },
+        {
+          totalPaymentPence: 8001,
+          paymentDate: '2027-06-07',
+          lineItems: [
+            {
+              parcelItemId: 1,
+              paymentPence: 1201,
+              _id: '68fa51d0206fcc31cb4b452c'
+            },
+            {
+              agreementLevelItemId: 1,
+              paymentPence: 6800,
+              _id: '68fa51d0206fcc31cb4b452d'
+            }
+          ],
+          _id: '68fa51d0206fcc31cb4b452b'
+        },
+        {
+          totalPaymentPence: 8001,
+          paymentDate: '2027-09-06',
+          lineItems: [
+            {
+              parcelItemId: 1,
+              paymentPence: 1201,
+              _id: '68fa51d0206fcc31cb4b452f'
+            },
+            {
+              agreementLevelItemId: 1,
+              paymentPence: 6800,
+              _id: '68fa51d0206fcc31cb4b4530'
+            }
+          ],
+          _id: '68fa51d0206fcc31cb4b452e'
+        },
+        {
+          totalPaymentPence: 8001,
+          paymentDate: '2027-12-06',
+          lineItems: [
+            {
+              parcelItemId: 1,
+              paymentPence: 1201,
+              _id: '68fa51d0206fcc31cb4b4532'
+            },
+            {
+              agreementLevelItemId: 1,
+              paymentPence: 6800,
+              _id: '68fa51d0206fcc31cb4b4533'
+            }
+          ],
+          _id: '68fa51d0206fcc31cb4b4531'
+        },
+        {
+          totalPaymentPence: 8001,
+          paymentDate: '2028-03-06',
+          lineItems: [
+            {
+              parcelItemId: 1,
+              paymentPence: 1201,
+              _id: '68fa51d0206fcc31cb4b4535'
+            },
+            {
+              agreementLevelItemId: 1,
+              paymentPence: 6800,
+              _id: '68fa51d0206fcc31cb4b4536'
+            }
+          ],
+          _id: '68fa51d0206fcc31cb4b4534'
+        },
+        {
+          totalPaymentPence: 8001,
+          paymentDate: '2028-06-05',
+          lineItems: [
+            {
+              parcelItemId: 1,
+              paymentPence: 1201,
+              _id: '68fa51d0206fcc31cb4b4538'
+            },
+            {
+              agreementLevelItemId: 1,
+              paymentPence: 6800,
+              _id: '68fa51d0206fcc31cb4b4539'
+            }
+          ],
+          _id: '68fa51d0206fcc31cb4b4537'
+        },
+        {
+          totalPaymentPence: 8001,
+          paymentDate: '2028-09-05',
+          lineItems: [
+            {
+              parcelItemId: 1,
+              paymentPence: 1201,
+              _id: '68fa51d0206fcc31cb4b453b'
+            },
+            {
+              agreementLevelItemId: 1,
+              paymentPence: 6800,
+              _id: '68fa51d0206fcc31cb4b453c'
+            }
+          ],
+          _id: '68fa51d0206fcc31cb4b453a'
+        }
+      ],
+      _id: '68fa51d0206fcc31cb4b4516'
+    },
+    applicant: {
+      business: {
+        name: 'J&S Hartley',
+        email: {
+          address: 'cliffspencetasabbeyfarmf@mrafyebbasatecnepsffilcm.com.test',
+          _id: '68fa51d0206fcc31cb4b453f'
+        },
+        phone: { mobile: '01234031670', _id: '68fa51d0206fcc31cb4b4540' },
+        address: {
+          line1: 'Mason House Farm Clitheroe Rd',
+          line2: 'Bashall Eaves',
+          line3: null,
+          line4: null,
+          line5: null,
+          street: 'Bartindale Road',
+          city: 'Clitheroe',
+          postalCode: 'BB7 3DD',
+          _id: '68fa51d0206fcc31cb4b4541'
+        },
+        _id: '68fa51d0206fcc31cb4b453e'
+      },
+      customer: {
+        name: {
+          title: 'Mr.',
+          first: 'Edward',
+          middle: 'Paul',
+          last: 'Jones',
+          _id: '68fa51d0206fcc31cb4b4543'
+        },
+        _id: '68fa51d0206fcc31cb4b4542'
+      },
+      _id: '68fa51d0206fcc31cb4b453d'
+    },
+    __v: 0,
+    createdAt: '2025-10-23T16:03:28.545Z',
+    updatedAt: '2025-10-23T16:03:28.548Z',
+    agreementNumber: 'SFI987654321',
+    invoice: [],
+    version: 1
+  }
+]
--- src/server/view-agreement/controller.test.js
+++ src/server/view-agreement/controller.test.js
@@ -1,40 +1,65 @@
+import path from 'node:path'
+
+import { Pact } from '@pact-foundation/pact'
+
 import { createServer } from '../server.js'
+import sampleData from '../common/helpers/sample-data/index.js'
+import { config } from '../../config/config.js'
 
 describe('#viewAgreementController', () => {
   let server
 
+  const provider = new Pact({
+    consumer: 'farming-grants-agreements-ui',
+    provider: 'farming-grants-agreements-api',
+    dir: path.resolve('src', 'contracts', 'consumer', 'pacts'),
+    pactfileWriteMode: 'update'
+  })
+
   beforeAll(async () => {
-    globalThis.fetch = vi.fn()
     server = await createServer()
     await server.initialize()
   })
 
   afterAll(async () => {
-    await server.stop({ timeout: 0 })
+    await server?.stop({ timeout: 0 })
   })
 
-  test('should call the backend API', async () => {
-    fetch.mockResolvedValueOnce({
-      ok: true,
-      json: async () => ({})
-    })
+  test('displays the agreement details page', async () => {
+    return await provider
+      .addInteraction()
+      .given('A customer has an accepted agreement offer')
+      .uponReceiving('a request from the customer to view their offer')
+      .withRequest('GET', '/SFI987654321', (builder) => {
+        builder.headers({ 'x-encrypted-auth': 'mock-auth' })
+      })
+      .willRespondWith(200, (builder) => {
+        builder.headers({ 'Content-Type': 'application/json' })
+        builder.jsonBody({
+          agreementData: { ...sampleData.agreements[0], status: 'accepted' }
+        })
+      })
+      .executeTest(async (mockServer) => {
+        config.set('backend.url', mockServer.url)
 
-    await server.inject({
-      method: 'POST',
-      url: '/SFI123456789',
-      headers: {
-        'x-encrypted-auth': 'mock-auth'
-      },
-      payload: {
-        action: 'view-agreement'
-      }
-    })
+        const { statusCode, result } = await server.inject({
+          method: 'POST',
+          url: '/SFI987654321',
+          headers: {
+            'x-encrypted-auth': 'mock-auth'
+          },
+          payload: {
+            action: 'view-agreement'
+          }
+        })
 
-    expect(fetch).toHaveBeenCalledWith('http://localhost:3555/SFI123456789', {
-      headers: {
-        'x-encrypted-auth': 'mock-auth'
-      },
-      method: 'GET'
-    })
+        expect(statusCode).toBe(200)
+        expect(result).toContain('Example agreement 2')
+        expect(result).toContain(
+          'You, J&amp;S Hartley, of Mason House Farm Clitheroe Rd, Bashall Eaves, Bartindale Road, Clitheroe, BB7 3DD'
+        )
+        expect(result).toContain('1 September 2025')
+        expect(result).toContain('£960.18')
+      })
   })
 })
