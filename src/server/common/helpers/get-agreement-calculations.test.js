3c7ff344c26aa3a0e9372c210609b34e340831d2
asifshaik04 <45974468+asifshaik04@users.noreply.github.com>
2025-12-15 14:53:57 +0000

    SFIR-767: Mssking dates for non accepted agreement- tests

--- src/server/common/helpers/get-agreement-calculations.test.js
+++ src/server/common/helpers/get-agreement-calculations.test.js
@@ -872,7 +872,9 @@ describe('getAgreementCalculations', () => {
       },
       { text: 'ACT1' },
       { text: 'Action One' },
-      { text: 2.5 }
+      { text: 2.5 },
+      { text: 'XXXXX' },
+      { text: 'XXXXX' }
     ])
 
     // Summary of payments should now have data (+ totals row)
