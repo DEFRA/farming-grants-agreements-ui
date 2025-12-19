98f94e10db1cced9c4f1cb8fd95adb473b64603e
Asif Shaik <45974468+asifshaik04@users.noreply.github.com>
2025-12-19 11:54:03 +0000

    SFIR-813: Added the missing pound sign (#79)
    * SFIR-813: Added the missing pound sign
    
    * SFIR-813: Added the missing pound sign

--- src/server/common/helpers/build-review-offer-model.js
+++ src/server/common/helpers/build-review-offer-model.js
@@ -53,7 +53,7 @@ export const buildReviewOfferModel = (agreementData) => {
     ...(Object.entries(payment?.agreementLevelItems || {}).map(([key, i]) => ({
       ...i,
       description: codeDescriptions[i.code],
-      rateInPence: `${formatPenceCurrency(i.annualPaymentPence)} per agreement`,
+      rateInPence: `${formatPenceCurrency(i.annualPaymentPence)}`,
       hasOneOffPayment: true,
       duration: durationInYears,
       quarterlyPayment: quarterlyPayment?.lineItems.find(
