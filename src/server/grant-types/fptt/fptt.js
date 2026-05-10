262565f0ebc6ebe0f6d25d483018e9ff59e08165
asifshaik04 <45974468+asifshaik04@users.noreply.github.com>
2026-05-11 00:18:03 +0100

    FGP-1036: Rebased review offer changes with main

--- src/server/grant-types/fptt/fptt.js
+++
@@ -1,9 +0,0 @@
-import { acceptOffer } from './accept-offer/accept-offer.js'
-import { offerAccepted } from './offer-accepted/offer-accepted.js'
-import { viewAgreement } from './view-agreement/view-agreement.js'
-
-export const fptt = {
-  acceptOffer,
-  offerAccepted,
-  viewAgreement
-}
--- src/server/grant-types/wmp/wmp.js
+++
@@ -1,9 +0,0 @@
-import { acceptOffer } from './accept-offer/accept-offer.js'
-import { offerAccepted } from './offer-accepted/offer-accepted.js'
-import { viewAgreement } from './view-agreement/view-agreement.js'
-
-export const wmp = {
-  acceptOffer,
-  offerAccepted,
-  viewAgreement
-}
