85dbd4b01673d57874636daaf421fed9c0820de6
asifshaik04 <45974468+asifshaik04@users.noreply.github.com>
2026-05-11 10:05:15 +0100

    FGP-1036: Remove commented code

--- src/server/grant-types/fptt/review-offer/review-offer.js
+++ src/server/grant-types/fptt/review-offer/review-offer.js
@@ -1,21 +1,6 @@
-// import { auditEvent, AuditEvent } from '#~/server/common/helpers/audit-event.js'
 import { buildReviewOfferModel } from '#~/server/common/helpers/build-review-offer-model.js'
 
 export const reviewOffer = {
   template: 'grant-types/fptt/review-offer/review-offer',
-  // validate(request, agreementData) {
-  //   if (request.payload?.confirm !== 'confirmed') {
-  //     auditEvent(
-  //       request,
-  //       AuditEvent.ACCEPT_OFFER_DECLARATION_NOT_CONFIRMED,
-  //       agreementData
-  //     )
-  //     return {
-  //       isValid: false,
-  //       errorMessage: 'Please agree with the Terms and Conditions'
-  //     }
-  //   }
-  //   return { isValid: true }
-  // },
   buildModel: ({ agreementData }) => buildReviewOfferModel(agreementData)
 }
